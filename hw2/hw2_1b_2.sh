b930e342ca5708f3055801f1c6bc301b9f94201f
x-kpatl5 <x-kpatel5@login06.anvil.rcac.purdue.edu>
2023-11-05 19:30:10 -0500

    Not the answers

--- hw2/hw2_1b_2.sh
+++ hw2/hw2_1b_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Number of times to execute the script
-total_executions=100
+total_executions=500
 
 # Output file to store the data
 output_file="hw2_1b_parallel.dat"
@@ -9,6 +9,6 @@ output_file="hw2_1b_parallel.dat"
 # Clear the output file if it exists (uncomment this if you want to start with a fresh file each time)
 # > "$output_file"
 
-# Use parallel to execute the script concurrently
-parallel -j "$(nproc)" bash parallel_1.sh '>>' "$output_file" ::: $(seq "$total_executions")
+# Use parallel to execute the script concurrently with 10 jobs at a time
+parallel -j 100 "bash hw2_1b_1.sh >> $output_file" ::: $(seq "$total_executions")
 
