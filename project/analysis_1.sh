a011c55be41afbfd78846d707a5c00ecec5986e3
Your Name <kpatel29@umassd.edu>
2023-12-17 03:14:01 +0000

    not the answers

--- project/analysis_1.sh
+++ project/analysis_1.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-
-
-# Loop through N values from 10^1 to 10^10
-for (( i=1; i<=12; i++ )); do
-    N=$((10**i))
     
-    # Loop through the number of threads from 1 to 10
-    for (( threads=10; threads<=100; threads+10 )); do
-        ./mcmc_parallel $N $threads >> "mcmc_parallel.dat"
-    done
+for (( threads=70; threads<=100; threads=threads+10 )); do
+    ./mcmc_parallel 1000000000000 $threads >> "mcmc_parallel.dat"
 done
-
--- project/problem_2.sh
+++ project/problem_2.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Number of times to execute the script
-total_executions=1
+total_executions=500
 
 # Output file to store the data
 
-output_file="problem_1a.dat"
+output_file="multi_iterations.dat"
 
 #echo "#N Error" >> $output_file
 # Clear the output file if it exists (uncomment this if you want to start with a fresh file each time)
 # > "$output_file"
 
-parallel -j 11 "bash problem_1a1.sh >> $output_file" ::: $(seq "$total_executions")
+parallel -j 100 "bash problem_1.sh >> $output_file" ::: $(seq "$total_executions")
 #bash problem_1a1.sh >> $output_file ::: $(seq "$total_executions")
 
 #oinclude <stdlib.h>
