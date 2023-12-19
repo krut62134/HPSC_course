0295f390be07455cac4cc061fc6664a1f1cc37ea
krut <krutjpatel99@gmail.com>
2023-12-19 04:54:48 -0500

    Lab 9 answers

--- lab9/helloMPI.c
+++ lab9/helloMPI.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include <mpi.h>
-
-int main(int argc, char **argv) {
-	int rank,size;
-
-	MPI_Init (&argc, &argv);
-	MPI_Comm_rank (MPI_COMM_WORLD, &rank);
-	MPI_Comm_size (MPI_COMM_WORLD, &size);
-	printf("Hello world from process%d of %d\n", rank, size );
-	MPI_Finalize();
-	return 0;
+#include <mpi.h> // include mpi library header
+int main(int argc, char **argv)
+{
+int rank, size;
+MPI_Init (&argc, &argv);
+ // initializes MPI
+MPI_Comm_rank (MPI_COMM_WORLD, &rank); // get current MPI-process ID. O, 1, ...
+MPI_Comm_size (MPI_COMM_WORLD, &size); // get the total number of processes
+printf( "Hello world from process %d of %d\n", rank, size );
+MPI_Finalize(); // programs should always perform a "graceful" shutdown
+return 0;
 }
+
--- lab9/optimizer.c
+++ lab9/optimizer.c
@@ -4,46 +4,50 @@
 #include <math.h>
 #include <mpi.h>
 
-void sample_rand(const double a, const double b, const int dim, double *x);
-double f(double *x);
+// will sample any dim-dimensional box of size [a,b]^dim
+// the random point is stored in the array x
+void sample_rand(const double a, const double b ,const int dim, double *x) {
 
-int main(int argc, char **argv) {
-  
-	int rank,size;
-  
-  	MPI_Init (&argc, &argv);
-  	MPI_Comm_rank (MPI_COMM_WORLD, &rank);
-  	MPI_Comm_size (MPI_COMM_WORLD, &size);
+  for(int i=0;i<dim;++i) {
+    double tmp = ((double) rand())/((double) RAND_MAX);
+    x[i] = (b-a)*tmp + a;
+  }
 
-  	long N = atoi( argv[1] );
-  	srand(time(NULL)*rank);
-  
-  	const int dim = 2;
-  	double x[dim];
-  
-  	double max = -1;
-  	for(long int i=1; i<=N; ++i) {
-    		sample_rand(-1.0, 1.0, dim, x);
-    		double f_i = f(x);
-    		if(f_i > max) {
-      			max = f_i;
-    		}
-    		printf("process %d: sample number %ld of %ld, f_i = %e\n", rank, i, N, f_i);
-  	}
-  	printf("process %d of %d local max = %1.5e\n", rank, size, max);
-  	MPI_Finalize();
-  	return 0;
 }
 
-void sample_rand(const double a, const double b, const int dim, double *x) {
-  	for(int i=0; i<dim; ++i){
-    		double tmp = ((double) rand())/((double) RAND_MAX);
-    		x[i] = (b - a)*tmp + a;
-  	}
+double logL(double x1,double x2) { 
+  return -(1.-x1)*(1.-x1) -100.*(x2-x1*x1)*(x2-x1*x1);
 }
 
-double f(double *x) {
-  	double x1 = x[0];
-  	double x2 = x[1];
-  	return exp( - (1. - x1)*(1. - x1) - 100.*(x2 - x1*x1)*(x2 - x1*x1));
+int main(int argc, char **argv)
+{
+  int rank, size;
+
+  MPI_Init (&argc, &argv);      // initializes MPI
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank); // get current MPI-process ID. O, 1, ...
+  MPI_Comm_size (MPI_COMM_WORLD, &size); // get the total number of processes
+
+  long N = atol( argv[1] ); 
+  srand(time(NULL) * rank); // each MPI process gets a unique seed
+
+  const int dim = 2;
+  double x[dim];  // array of random numbers
+
+
+  // search for the function's maximum
+  double max = -1;
+  for(long int i=1; i<=N; ++i) {
+    sample_rand(-1.,1.,dim,x);
+    double f_i = exp(logL(x[0],x[1]));
+    if( f_i > max) {
+      max = f_i;
+    }
+    //printf("Process %d: sample number %ld of %ld, f_i = %e\n",rank, i, N, f_i);
+  }
+
+  printf("Process %d of %d local max = %1.5e\n", rank, size, max );
+
+
+  MPI_Finalize(); // programs should always perform a "graceful" shutdown
+  return 0;
 }
